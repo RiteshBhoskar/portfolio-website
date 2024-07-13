c1a2d298c755c3ff38dc63dac194bcfa0d74af69
Ritesh Bhoskar <riteshbhoskar7@gmail.com>
2024-07-13 09:58:37 +0530

    Changed the Header responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,25 +15,28 @@ export default function Header() {
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 
-        left-1/2 h-[4.3rem] w-full
+        left-1/2 h-14 w-full
         rounded-none 
          border border-white border-opacity-40 -translate-x-1/2 bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:h-[2.80rem] sm:w-[36rem] sm:rounded-full sm:top-6 sm:block"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className=" fixed left-1/2 h-12 -translate-x-1/2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-fit items-center justify-center flex-wrap h-fit gap-y-1 text-[0.9rem] font-lato font-medium text-cyan-300  sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav
+        className=" fixed left-1/2 w-full h-fit
+      py-1 -translate-x-1/2 sm:top-[1.7rem] sm:h-12 sm:py-0"
+      >
+        <ul className="flex w-full items-center justify-center flex-wrap h-fit gap-y-1 text-[0.9rem] font-lato font-medium text-cyan-300  sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               key={link.hash}
-              className="h-3/4 flex items-center justify-center relative"
+              className="sm:h-3/4 flex h-fit items-center justify-center relative"
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
-                  `flex w-full items-center justify-center
-                   py-1                  px-2 sm:px-3 sm:py-2 hover:text-white
+                  `flex w-full items-center justify-center h-fit
+                               px-3 sm:px-3 sm:py-2 hover:text-white
                                 transition`,
                   { "text-white": activeSection === link.name }
                 )}
@@ -46,7 +49,6 @@ export default function Header() {
           ))}
         </ul>
       </nav>
-
       {/* ADD THE DROPDOWN MENU */}
 
       {/* <div className="text-white flex justify-end pt-9 pr-3">
