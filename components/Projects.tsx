3e4695b0d441d2e05028cabf8c4a1d51c22f2388
RiteshBhoskar <riteshbhoskar7@gmail.com>
2025-04-17 18:58:33 +0530

    show more button added

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,15 @@
 "use client"
 import { projectsData } from "@/lib/data";
-import React from "react";
+import React, { useState } from "react";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hook";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects");
+  const [showAll, setShowAll] = useState(false);
+
+  const VISIBLE_COUNT = 6;
+  const visibleProjects = showAll ? projectsData : projectsData.slice(0, VISIBLE_COUNT);
   
   return (
     <section ref={ref} className="flex flex-col items-center justify-center" id="projects">
@@ -13,7 +17,7 @@ export default function Projects() {
         My Projects
       </h2>
       <div>
-        {projectsData.map((project , index)=>(
+        {visibleProjects.map((project , index)=>(
           <div
           className="flex flex-col justify-center
           mb-5 mx-3"
@@ -22,6 +26,15 @@ export default function Projects() {
           </div>
         ))}
       </div>
+
+      {projectsData.length > VISIBLE_COUNT && (
+        <button
+          onClick={() => setShowAll((prev) => !prev)}
+          className="my-6 px-4 py-2 bg-transparent border-white border backdrop-blur-3xl text-white rounded-full "
+        >
+          {showAll ? "Show Less" : "Show More"}
+        </button>
+      )}
     </section>
   )
 };
